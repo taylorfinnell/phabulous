f5254ac361157518fa459cec725a41d913f626de
Santiago Doldan <santiagodolte@gmail.com>
2017-10-03 19:40:20 -0300

    Fix console and add 2 new methods to find revisions

--- Gemfile
+++ Gemfile
@@ -1,5 +1,15 @@
 source 'https://rubygems.org'
 
+group :development do
+  gem 'dotenv', '~> 2.2.1'
+
+  if RUBY_VERSION < '2.0.0'
+    gem 'pry', '~> 0.10'
+  else
+    gem 'pry-byebug', '~> 3.4.3'
+  end
+end
+
 group :test do
   gem 'webmock', '~> 3.0.1'
 end
--- lib/phabulous/revision.rb
+++ lib/phabulous/revision.rb
@@ -2,8 +2,39 @@ require 'uri'
 
 module Phabulous
   class Revision < Entity
+    PREFIX = 'D'
+
+    NEEDS_REVIEW    = 'Needs Review'
+    CLOSED          = 'Closed'
+    ACCEPTED        = 'Accepted'
+    ABANDONED       = 'Abandoned'
+    CHANGES_PLANNED = 'Changes Planned'
+    NEEDS_REVISION  = 'Needs Revision'
+
     attr_accessor :id, :title, :uri, :dateModified, :authorPHID,
-      :reviewers, :statusName, :lineCount, :summary, :statusName
+                  :reviewers, :statusName, :lineCount, :summary, :statusName
+
+    class << self
+      # @param [String, Integer] id.
+      # @return [Phabulous::Revision]
+      def find_by_differential_id(id)
+        id.sub!(PREFIX, '')
+
+        all.select { |entity| entity.id == id }.first
+      end
+
+      # @param [Array<String>] statuses.
+      # @return [Array<Phabulous::Revision>]
+      def by_status(*statuses)
+        all.select { |entity| statuses.include?(entity.status) }
+      end
+
+      protected
+
+      def conduit_name
+        'differential'
+      end
+    end
 
     def status
       @statusName
@@ -27,12 +58,6 @@ module Phabulous
 
     def reviewers
       @reviewers.collect { |reviewier| User.find(reviewier) }
-    end
-
-    protected
-
-    def self.conduit_name
-      'differential'
     end
   end
 end
